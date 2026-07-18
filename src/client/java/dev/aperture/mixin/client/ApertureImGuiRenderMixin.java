cba06288772e73bc703a655d9bf91da6018c9754
Aperture Dev <aperture@dev.local>
2026-07-19 00:26:48 +0800

    refactor(render): 更新 ImGui 渲染方法签名以匹配新参数
    - 添加 TracyFrameCapture 参数到 flipFrame 方法注入
    - 修改 aperture$renderDearImGui 方法以接收新的 frameCapture 参数
    - 保持原有的 Dear ImGui 渲染功能不变
    - 适配 RenderSystem.flipFrame 方法的签名变化

--- src/client/java/dev/aperture/mixin/client/ApertureImGuiRenderMixin.java
+++ src/client/java/dev/aperture/mixin/client/ApertureImGuiRenderMixin.java
@@ -1,5 +1,6 @@
 package dev.aperture.mixin.client;
 
+import com.mojang.blaze3d.TracyFrameCapture;
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.aperture.client.editor.imgui.ApertureImGuiClient;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderSystem.class)
 public abstract class ApertureImGuiRenderMixin {
-	@Inject(method = "flipFrame", at = @At("HEAD"))
-	private static void aperture$renderDearImGui(CallbackInfo ci) {
+	@Inject(method = "flipFrame(Lcom/mojang/blaze3d/TracyFrameCapture;)V", at = @At("HEAD"))
+	private static void aperture$renderDearImGui(TracyFrameCapture frameCapture, CallbackInfo ci) {
 		ApertureImGuiClient.renderPendingDrawData();
 	}
 }
